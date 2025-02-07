bc8fd54fe1e9466afaab1507a6eaa16f9452cc66
Mohd Amaan <mohd.amaan8642@gmail.com>
2025-02-07 09:47:58 +0530

    FileSystem Operation

--- AIML/Backend/Filesystem/file.js
+++ AIML/Backend/Filesystem/file.js
@@ -1,3 +1,23 @@
 const fs = require('fs');
-const data = fs.readFileSync('data', 'utf-8');
-console.log(data.toString());
+function printFile(fileName){
+    const data = fs.readFileSync(fileName, 'utf-8');
+    console.log(data.toString());
+}
+
+printFile('data')
+const txt = "Hello World\n";
+fs.writeFileSync('tmp', txt);
+
+printFile('tmp')
+
+const newTxt = "Hello Node";
+fs.appendFileSync('tmp', newTxt);
+
+printFile('tmp')
+
+if(newTxt.match('H')){
+    const newData = newTxt.replace('H', 'J');
+    fs.writeFileSync('tmp', newData);
+}
+
+printFile('tmp')
